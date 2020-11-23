48146627e20088136d46da17c840cb52948bb58b
Portgam <38985295+Portgam@users.noreply.github.com>
2020-11-23 17:53:50 -0600

    caja2
    La aplicación muestra bien la caja 2

--- ClienteFTP/Headers/VentanaPrincipal.h
+++ ClienteFTP/Headers/VentanaPrincipal.h
@@ -6,15 +6,22 @@
 class W_principal : public Gtk::Window {
 private:
     Gtk::Box b_cajaPrincipal;
+    /// Caja 1
     Gtk::Box b_caja1;
     Gtk::Label l_host;
     Gtk::Label l_puerto;
     Gtk::Entry e_host;
     Gtk::Entry e_puerto;
     Gtk::Button b_connectar;
+
+    /// Caja 2
+    Gtk::Box b_caja2;
+    Gtk::Entry e_mensajes;
+
 public:
     W_principal();
     void set_caja1();
+    void set_caja2();
 };
 
 
--- ClienteFTP/Sources/VentanaPrincipal.cpp
+++ ClienteFTP/Sources/VentanaPrincipal.cpp
@@ -4,8 +4,13 @@
 
 W_principal::W_principal(){
     this->add(b_cajaPrincipal);
+    b_cajaPrincipal.set_orientation(Gtk::Orientation::ORIENTATION_VERTICAL);
+
     b_cajaPrincipal.add(b_caja1);
+    b_cajaPrincipal.add(b_caja2);
+
     set_caja1();
+    set_caja2();
 
     this->show_all_children();
     this->set_title("Cliente FTP");
@@ -23,4 +28,9 @@ void W_principal::set_caja1(){
     l_host.set_label("Host");
     l_puerto.set_label("Puerto");
     b_connectar.set_label("Conectar");
+}
+
+void W_principal::set_caja2(){
+    b_caja2.add(e_mensajes);
+    e_mensajes.set_text("Hola Mundo");
 }
